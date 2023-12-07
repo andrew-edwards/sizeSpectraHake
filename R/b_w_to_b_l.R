36cf33dad7f1560a80321d59f7acf023949e68f8
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2023-12-07 10:06:30 -0800

    Add three functions, document().

---
+++ R/b_w_to_b_l.R
@@ -0,0 +1,33 @@
+##' Converting between length- and weight-based size spectrum exponents
+
+##' Will write out the math in a write up, but the equations are:
+##' b_l = \beta (b_w + 1) - 1
+##' b_w = (b_l + 1) / \beta - 1
+##' @param b_l exponent for a length-based individual size spectrum
+##' @param beta exponent of the length-weight relationship for the species in
+# question
+##' @return the `b_w` exponent for the weight-based individual size spectrum
+##' @export
+##' @author Andrew Edwards
+##' @examples
+##' \dontrun{
+##' b_weight <- b_l_to_b_w(-5.1, beta = 3)
+##' }
+b_l_to_b_w <- function(b_l,
+                       beta = 3.1802){
+ (b_l + 1) / beta - 1
+}
+
+##' @inheritParams b_l_to_b_w
+##' @param b_w exponent for a weight-based individual size spectrum
+##' @return the `b_l` exponent for the length-based individual size spectrum
+##' @export
+##' @author Andrew Edwards
+##' @examples
+##' \dontrun{
+##' b_length <- b_w_to_b_l(-2, beta = 3)
+##' }
+b_w_to_b_l <- function(b_w,
+                       beta = 3.1802){
+  beta * (b_w + 1) - 1
+}
---
+++ R/f.R
@@ -0,0 +1,19 @@
+#' Format a number for write up.
+#'
+#' @details
+#' The formatted number will have comma-separated thousands, millions, etc.
+#' and the exact number of decimal points given by `digits`, even if the
+#' last one is a zero.
+#'
+#' @param x The number
+#' @param digits The number of decimal points to use
+#'
+#' @return A Character string. A formatted version of the number `x`.
+#' @export
+#' @author Chris Grandin (in hake assessment)
+f <- function(x, digits = 0){
+  format(round(x,
+               digits),
+         big.mark = ",",
+         nsmall = digits)
+}
